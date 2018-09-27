b14561e37672002d0bb34cb5f90d292913455d22
Keith Smith <KeithSSmith@users.noreply.github.com>
2018-09-27 02:47:02 -0500

    Update docker_openvpn_run.sh

--- docker_openvpn_run.sh
+++ docker_openvpn_run.sh
@@ -7,7 +7,7 @@ docker rm openvpn
 
 docker run -d \
   --name openvpn \
-  -p 36036:1194/udp \
+  -p 1194:1194/udp \
   -v ${OVPN_DATA}/openvpn_data/:/etc/openvpn \
   --restart=unless-stopped \
   --cap-add=NET_ADMIN \
