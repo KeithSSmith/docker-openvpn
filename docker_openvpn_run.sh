69fbbc983dd883fdbb98ce7ddc9dab15639bf79f
Keith Smith <KeithSSmith@users.noreply.github.com>
2018-09-27 03:02:23 -0500

    Update docker_openvpn_run.sh

--- docker_openvpn_run.sh
+++ docker_openvpn_run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 OVPN_DATA=$(pwd)
+OVPN_PORT=1194
+DOCKER_HOST_PORT=1194
 
 docker stop openvpn
 docker rm openvpn
 
 docker run -d \
   --name openvpn \
-  -p 1194:1194/udp \
+  -p ${DOCKER_HOST_PORT}:${OVPN_PORT}/udp \
   -v ${OVPN_DATA}/openvpn_data/:/etc/openvpn \
   --restart=unless-stopped \
   --cap-add=NET_ADMIN \
